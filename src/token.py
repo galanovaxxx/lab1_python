facbef0302587e305833405a98aaf8d71d3d55c0
Galanova Ekaterina <159624387+galanovaxxx@users.noreply.github.com>
2025-10-02 09:24:04 +0300

    Create token.py
    Функция для токенизации. Составляет список из чисел и знаков и переводит числа в int или float.

---
+++ src/token.py
@@ -0,0 +1,28 @@
+def token_function(s):
+    """Функция для токенизации. Составляет список из чисел и знаков и
+    переводит числа в int или float."""
+    s = s.replace(' ', '')
+    s = [x for x in s]
+    i = 1
+    while i < len(s):
+        if s[i - 1][0] in "0123456789.":
+            if s[i] in "0123456789.":
+                s[i - 1] = s[i - 1] + s[i]
+                s.pop(i)
+            else:
+                i += 1
+        elif s[i] in "*/":
+            if s[i - 1] in "*/":
+                s[i - 1] = s[i] + s[i - 1]
+                s.pop(i)
+            else:
+                i += 1
+        else:
+            i += 1
+    for i in range(len(s)):
+        if '.' in s[i]:
+            s[i] = float(s[i])
+        elif '0' in s[i] or '1' in s[i] or '2' in s[i] or '3' in s[i] or '4' in s[i] or '5' in s[i] or '6' in s[
+            i] or '7' in s[i] or '8' in s[i] or '9' in s[i]:
+            s[i] = int(s[i])
+    return s

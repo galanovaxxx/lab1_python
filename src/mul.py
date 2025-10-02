e017e124877c563a4d6c918ef43ed575936e74e9
Galanova Ekaterina <159624387+galanovaxxx@users.noreply.github.com>
2025-10-02 09:13:23 +0300

    Create mul.py
    Функция находит '*', или '/', или '//', или '%', находящееся не в скобках и снова вызывает эту функцию от частей списка до и после найденных элементов. Если элементов в списке нет, вызывает другую функцию.

---
+++ src/mul.py
@@ -0,0 +1,57 @@
+from src.pow import pow_function
+
+
+def mul_function(s):
+    """Функция находит '*', или '/', или '//', или '%' аходящееся не в скобках и
+    снова вызывает эту функцию от частей списка до и после найденых элементов.
+    Если элементов в списке нет, вызывает другую функцию."""
+    if '*' in s:
+        balance = 0
+        for x in range(len(s)):
+            if s[x] == '(':
+                balance += 1
+            if s[x] == ')':
+                balance -= 1
+            if s[x] == '*':
+                if balance == 0:
+                    return mul_function(s[:x]) * mul_function(s[x + 1:])
+    if '/' in s:
+        balance = 0
+        for x in range(len(s)):
+            if s[x] == '(':
+                balance += 1
+            if s[x] == ')':
+                balance -= 1
+            if s[x] == '/':
+                if balance == 0:
+                    if mul_function(s[x + 1:]) != 0:
+                        return mul_function(s[:x]) / mul_function(s[x + 1:])
+                    else:
+                        raise ValueError("Ошибка при делении")
+    if '//' in s:
+        balance = 0
+        for x in range(len(s)):
+            if s[x] == '(':
+                balance += 1
+            if s[x] == ')':
+                balance -= 1
+            if s[x] == '//':
+                if balance == 0:
+                    if mul_function(s[x + 1:]) != 0 and int(mul_function(s[:x])) == mul_function(s[:x]) and int(mul_function(s[x + 1:])) == mul_function(s[x + 1:]):
+                        return mul_function(s[:x]) // mul_function(s[x + 1:])
+                    else:
+                        raise ValueError("Ошибка при делении")
+    if '%' in s:
+        balance = 0
+        for x in range(len(s)):
+            if s[x] == '(':
+                balance += 1
+            if s[x] == ')':
+                balance -= 1
+            if s[x] == '%':
+                if balance == 0:
+                    if mul_function(s[x + 1:]) != 0 and int(mul_function(s[:x])) == mul_function(s[:x]) and int(mul_function(s[x + 1:])) == mul_function(s[x + 1:]):
+                        return mul_function(s[:x]) % mul_function(s[x + 1:])
+                    else:
+                        raise ValueError("Ошибка при делении")
+    return pow_function(s)

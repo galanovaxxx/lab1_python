ea52948989d63cd65a069a6b48d92263b2c847f6
Galanova Ekaterina <159624387+galanovaxxx@users.noreply.github.com>
2025-10-02 09:16:22 +0300

    Update and rename power.py to pow.py
    Функция находит '**', находящееся не в скобках и снова вызывает эту функцию от частей списка до и после '**'. Если '**' нет в списке, вызывает функцию unary.

---
+++ src/pow.py
@@ -0,0 +1,18 @@
+from src.unary import unary_function
+
+
+def pow_function(s):
+    """Функция находит '**', находящееся не в скобках и снова вызывает эту функцию
+    от частей списка до и после '**'. Если '**' нет в списке, вызывает другую функцию"""
+    if '**' in s:
+        balance = 0
+        for x in range(len(s)):
+            if s[x] == '(':
+                balance += 1
+            if s[x] == ')':
+                balance -= 1
+            if s[x] == '**':
+                if balance == 0:
+                    return pow_function(s[:x]) ** pow_function(s[x + 1:])
+
+    return unary_function(s)

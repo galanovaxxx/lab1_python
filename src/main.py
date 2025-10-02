47d7d7a59b079edd7dd8b85617f5c25028c83744
Galanova Ekaterina <159624387+galanovaxxx@users.noreply.github.com>
2025-10-02 09:29:24 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,20 +1,9 @@
-from src.power import power_function
-from src.constants import SAMPLE_CONSTANT
-
+from src.final import final_function
 
 def main() -> None:
-    """
-    Обязательнная составляющая программ, которые сдаются. Является точкой входа в приложение
-    :return: Данная функция ничего не возвращает
-    """
+    s = input()
 
-    target, degree = map(int, input("Введите два числа разделенные пробелом: ").split(" "))
-
-    result = power_function(target=target, power=degree)
-
-    print(result)
-
-    print(SAMPLE_CONSTANT)
+    print(final_function(s))
 
 if __name__ == "__main__":
     main()

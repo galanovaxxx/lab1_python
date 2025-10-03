6c364979bad042f9f1e0e37671b5ce2c6507d3ad
Galanova Ekaterina <159624387+galanovaxxx@users.noreply.github.com>
2025-10-03 10:15:54 +0300

    Update final.py

--- src/final.py
+++ src/final.py
@@ -3,7 +3,7 @@ import src.token
 import src.expr
 
 
-def final_function(s: list) -> int, float:
+def final_function(s: list) -> [int, float]:
     if src.check.check_function(s):
         s = src.token.token_function(s)
         k = src.expr.expr_function(s)

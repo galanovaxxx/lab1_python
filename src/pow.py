3890beef7f41fa8fcc75edfafdcd1ed4e9c0253e
Galanova Ekaterina <159624387+galanovaxxx@users.noreply.github.com>
2025-10-04 11:34:53 +0300

    Update pow.py

--- src/pow.py
+++ src/pow.py
@@ -4,7 +4,7 @@ import src.unary
 def pow_function(s: list) -> [int, float]:
     """Функция находит '**', находящееся не в скобках и снова вызывает эту функцию
     от частей списка до и после '**'. Если '**' нет в списке, вызывает другую функцию"""
-    if '**' in s:
+    if '**' in s: #если в списке есть **
         balance = 0
         for x in range(len(s)):
             if s[x] == '(':
@@ -12,7 +12,7 @@ def pow_function(s: list) -> [int, float]:
             if s[x] == ')':
                 balance -= 1
             if s[x] == '**':
-                if balance == 0:
-                    return pow_function(s[:x]) ** pow_function(s[x + 1:])
+                if balance == 0: # если ** не в скобках
+                    return pow_function(s[:x]) ** pow_function(s[x + 1:]) # вызываем эту же функцию до и после **
 
-    return src.unary.unary_function(s)
+    return src.unary.unary_function(s) # если нет ** вызываем другую функцию

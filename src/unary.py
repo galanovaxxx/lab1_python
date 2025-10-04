8f0dfa0ccc6620ff582d518c5079959e6ad842e7
Galanova Ekaterina <159624387+galanovaxxx@users.noreply.github.com>
2025-10-04 11:41:34 +0300

    Update unary.py

--- src/unary.py
+++ src/unary.py
@@ -6,22 +6,11 @@ def unary_function(s: list) -> [int, float]:
     """Функция работает с унарными знаками. Если '+', удаляет его, если '-', удаляет его
     и умножает следующее за ним число на (-1)."""
     print(s)
-    if s:
-        if s[0] == '+':
-            s.pop(0)
-            return src.expr.expr_function(s)
-        if s[0] == '-':
-            s.pop(0)
-            return -(src.expr.expr_function(s))
-        if s:
-            x = 0
-            while x > len(s):
-                if s[x] == '(':
-                    if s[x + 1] == '+':
-                        s.pop(x + 1)
-                    if s[x + 1] == '-':
-                        s.pop(x + 1)
-                        s[x + 1] = s[x + 1] * (-1)
-                    x += 1
-    print(s)
-    return src.primary.primary_function(s)
+    if s: # если s не пустой 
+        if s[0] == '+': # если первый символ +
+            s.pop(0) # удаляем +
+            return src.expr.expr_function(s) # вызываем функцию снова
+        if s[0] == '-': # если первый символ -
+            s.pop(0) # удаляем -
+            return -(src.expr.expr_function(s)) # вызываем отрицательную функцию от этой функции
+    return src.primary.primary_function(s) # вызываем следующую функцию
